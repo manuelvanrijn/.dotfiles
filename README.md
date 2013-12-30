c7552d3cfc292d5e9f84aaf5d8fa1e17ed8fe46d
Manuel van Rijn <manuel@manuelles.nl>
2013-12-30 13:17:40 +0100

    some new settings

--- README.md
+++ README.md
@@ -38,3 +38,7 @@ git clone https://github.com/manuelvanrijn/.dotfiles.git ~/.dotfiles
 cd ~/.dotfiles
 rake
 ```
+
+#### Quicklook plugins
+
+see: [quick-look-plugins](https://github.com/sindresorhus/quick-look-plugins)
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,2 +1,10 @@
 alias reload!='. ~/.zshrc'
 alias pp='python -mjson.tool'
+
+# npm install -g npm-check-updates
+alias npmui='npm-check-updates -u && npm install'
+
+# vagrant
+alias vagu='vagrant up --no-provision'
+alias vagh='vagrant halt'
+alias vagd='vagrant destroy'
