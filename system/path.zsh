69b5f73a08bfbc2a2ac997e0afc2a606ec862099
Manuel van Rijn <manuel@manuelvanrijn.nl>
2014-06-05 00:48:10 +0200

    some cleanup

--- system/path.zsh
+++ system/path.zsh
@@ -1,8 +1,15 @@
-export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$HOME/.rbenv/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin:/usr/local/git/bin:$PATH"
-export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+# export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$HOME/.rbenv/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin:/usr/local/git/bin:$PATH"
+# export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+#
+# export PATH="$PATH:/Library/Frameworks/Python.framework/Versions/3.2/bin"
+# export PATH="$PATH:/usr/local/heroku/bin"
+#
+# export PATH="$PATH:/Applications/android-dev/sdk/platform-tools"
+# export PATH="$PATH:/Applications/android-dev/sdk/tools"
 
-export PATH="$PATH:/Library/Frameworks/Python.framework/Versions/3.2/bin"
-export PATH="$PATH:/usr/local/heroku/bin"
+export PATH="/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin"
+# brew
+export PATH="/usr/local/bin:$PATH"
 
-export PATH="$PATH:/Applications/android-dev/sdk/platform-tools"
-export PATH="$PATH:/Applications/android-dev/sdk/tools"
+# some random apps loaded from ~/.bin
+export PATH="$PATH:$HOME/.bin"
