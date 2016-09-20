35d70345fd145953f4fa4761a4ef085f867e4e0f
Manuel van Rijn <manuel@manuelvanrijn.nl>
2016-09-20 22:29:18 +0200

    more awesome

--- system/path.zsh
+++ system/path.zsh
@@ -1,12 +1,3 @@
-# export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$HOME/.rbenv/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin:/usr/local/git/bin:$PATH"
-# export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
-#
-# export PATH="$PATH:/Library/Frameworks/Python.framework/Versions/3.2/bin"
-# export PATH="$PATH:/usr/local/heroku/bin"
-#
-# export PATH="$PATH:/Applications/android-dev/sdk/platform-tools"
-# export PATH="$PATH:/Applications/android-dev/sdk/tools"
-
 export PATH="/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin"
 # dotfiles bin
 export PATH="$PATH:$DOTFILES/bin"
@@ -14,15 +5,5 @@ export PATH="$PATH:$DOTFILES/bin"
 export PATH="/usr/local/bin:$PATH"
 # go
 export PATH="$PATH:/usr/local/opt/go/libexec/bin"
-
-# lilypond
-export PATH="$PATH:/Applications/LilyPond.app/Contents/Resources/bin"
-
 # some random apps loaded from ~/.bin
 export PATH="$PATH:$HOME/.bin"
-
-# chefdk
-export PATH="$PATH:/opt/chefdk/bin"
-
-# postgres.app
-export PATH="$PATH:/Applications/Postgres.app/Contents/Versions/9.4/bin"
