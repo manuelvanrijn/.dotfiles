6b84753cdbba7a2486a80295a04bd99c1cb0ecb8
Manuel van Rijn <manuel@manuelvanrijn.nl>
2022-12-02 15:49:53 +0100

    Removed imagemagick@6

--- system/env.zsh
+++ system/env.zsh
@@ -4,8 +4,4 @@ export EDITOR='code'
 export LC_CTYPE=en_US.UTF-8
 export LANG=en_US.UTF-8
 
-# issue with imagemagick@6
-export PKG_CONFIG_PATH='/usr/local/Cellar/imagemagick@6/6.9.9-23/lib/pkgconfig'
-export PATH="/usr/local/opt/imagemagick@6/bin:$PATH"
-
 export RUBY_CONFIGURE_OPTS=--with-jemalloc
